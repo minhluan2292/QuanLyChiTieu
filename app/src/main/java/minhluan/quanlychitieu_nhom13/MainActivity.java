aa71d9419e6d50b7a213d61f79b58ebbd9b5eead
GeniusHCMUS <minhluan2292@gmail.com>
2017-01-14 09:49:55 -0800

    Commit first

---
+++ app/src/main/java/minhluan/quanlychitieu_nhom13/MainActivity.java
@@ -0,0 +1,13 @@
+package minhluan.quanlychitieu_nhom13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
